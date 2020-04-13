e0e2ef5e8d1123f77dd8202a9fab9b789b31ee57
strogiyotec <almas337519@gmail.com>
2020-04-12 21:26:49 -0700

    add test file

--- src/main/resources/db/migration/V2__createTable.sql
+++ src/main/resources/db/migration/V2__createTable.sql
@@ -6,3 +6,11 @@ CREATE TABLE translations (
   translated_text TEXT,
   created_at      TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
+
+
+CREATE TABLE USERS (
+  id   SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+INSERT INTO USERS (name) VALUES ('Hello'), ('My friend');
